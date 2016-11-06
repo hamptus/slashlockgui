8350bb21c68e127d6fc490d5cb00074be7d9808d
Doc Tart <doc.tart@wookalar.io>
2016-11-06 15:29:56 -0500

    Updating version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='slashlockgui',
-    version='0.3.0',
+    version='0.3.1',
     description='SlashlockGUI - A simple GUI for Slashlock',
     url='https://github.com/wookalar/slashlockgui',
     author='Doc Tart',
