dfa0a7bf4612f7f602cf197a541cc43a364571d5
Doc Tart <doc.tart@wookalar.io>
2016-11-06 14:42:43 -0500

    Updating to version 0.3.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='slashlockgui',
-    version='0.2.3',
+    version='0.3.0',
     description='SlashlockGUI - A simple GUI for Slashlock',
     url='https://github.com/wookalar/slashlockgui',
     author='Doc Tart',
     author_email='doc.tart@wookalar.io',
     license='AGPLv3',
     entry_points={'gui_scripts': [
-        'slashgui = slashlock.gui:main',
+        'slashlockgui = slashlockgui.gui:main',
     ]},
     setup_requires=[
         'Cython>=0.24.1',
@@ -21,8 +21,8 @@ setup(
         'Kivy-Garden==0.1.4',
     ],
     install_package_data=True,
-    py_modules=['slashlockgui'],
+    packages=['slashlockgui'],
     package_data={
-        'slashlock': ['gui.kv'],
+        'slashlockgui': ['kvs/*.kv'],
     }
 )
